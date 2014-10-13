3316ce8f7f247fa661743579fa1455e6912543c8
abdullahyaprak27 <abdullahyaprak1@gmail.com>
2014-10-13 21:48:24 +0300

    ReplaceUnderscoreWithSpace

--- ReplaceUnderscoreWithSpace.py
+++ ReplaceUnderscoreWithSpace.py
@@ -1,28 +1,29 @@
 def ReplaceUnderscoreWithSpace( string ):
-    liste = list(string)
+    liste = list(string) 
     first_index = FirstAndLastControl(liste)
     last_index = FirstAndLastControl(liste[::-1])
     character_control = "".join(liste[first_index:len(liste)-last_index]).replace("_"," ")
+
     for i in range(len(character_control)):
         liste[first_index+i] = character_control[i]
 
+    return "".join(liste)
 
-
-
-              
-    return "".join(liste) 
 def Control(harf):
+    
     if harf == "_":
         return False
     else:
         return True
         
 def FirstAndLastControl(liste):
+    
     state = Control(liste[0])
+    
     for i in range(len(liste)):
-        if liste[i]=="_" and state==False:
+        if liste[i] == "_" and state==False:
             pass
-        elif liste[i]!="_" :
+        elif liste[i] != "_" :
             return i
         
     
